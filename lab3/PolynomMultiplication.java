285fa5155e4f25af64732d401a4eb2eb6f0ac5d2
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2019-09-15 23:54:12 +0530

    tried fft

--- lab3/PolynomMultiplication.java
+++ lab3/PolynomMultiplication.java
@@ -6,176 +6,187 @@ import java.io.*;
  */
 
 class ComplexNumber {
-  double real;
-  double img;
+	int real;
+	int img;
 
-  ComplexNumber() {
-    this.real = 0;
-    this.img = 0;
-  }
+	ComplexNumber() {
+		this.real = 0;
+		this.img = 0;
+	}
 
-  ComplexNumber(double real, double img) {
-    this.real = real;
-    this.img = img;
-  }
+	ComplexNumber(double real, double img) {
+		this.real = (int) real;
+		this.img = (int) img;
+	}
 
-  ComplexNumber mul(ComplexNumber c) {
-    ComplexNumber res = new ComplexNumber();
+	ComplexNumber mul(ComplexNumber c) {
+		ComplexNumber res = new ComplexNumber();
 
-    res.real = (this.real * c.real) - (this.img * c.img);
-    res.img = (this.real * c.img) + (this.img * c.real);
+		res.real = (this.real * c.real) - (this.img * c.img);
+		res.img = (this.real * c.img) + (this.img * c.real);
 
-    return res;
-  }
+		return res;
+	}
 
-  ComplexNumber add(ComplexNumber c) {
-    ComplexNumber res = new ComplexNumber();
+	ComplexNumber add(ComplexNumber c) {
+		ComplexNumber res = new ComplexNumber();
 
-    res.real = (this.real + c.real);
-    res.img = (this.real + c.img);
+		res.real = (this.real + c.real);
+		res.img = (this.real + c.img);
 
-    return res;
-  }
+		return res;
+	}
 
-  ComplexNumber sub(ComplexNumber c) {
-    ComplexNumber res = new ComplexNumber();
+	ComplexNumber sub(ComplexNumber c) {
+		ComplexNumber res = new ComplexNumber();
 
-    res.real = (this.real - c.real);
-    res.img = (this.real - c.img);
+		res.real = (this.real - c.real);
+		res.img = (this.real - c.img);
 
-    return res;
-  }
+		return res;
+	}
 
 }
 
 public class PolynomMultiplication {
 
-  static InputStreamReader ir = new InputStreamReader(System.in);
-  static BufferedReader br = new BufferedReader(ir);
+	static InputStreamReader ir = new InputStreamReader(System.in);
+	static BufferedReader br = new BufferedReader(ir);
 
-  static void printArray(ComplexNumber[] arr) {
-    for(ComplexNumber el: arr) System.out.print(el.real + " ");
-    System.out.println();
-  }
+	static void printArray(ComplexNumber[] arr) {
+		for (ComplexNumber el : arr)
+			System.out.print("(" + el.real + " " + el.img + ") ");
+		System.out.println();
+	}
 
-  static int getExtendedSize(int deg) {
+	static int getExtendedSize(int deg) {
 
-    int n = (int) (Math.log10(deg - 1) / Math.log10(2));
-    int extendedSize = (int) Math.pow(2, n + 1) + 1;
+		int n = (int) (Math.log10(deg) / Math.log10(2));
+		int extendedSize = (int) Math.pow(2, n + 1);
 
-    return extendedSize;
-  }
+		return extendedSize;
+	}
 
-  static ComplexNumber[] getPolynomials(int extendedSize) throws IOException {
-    
-    ComplexNumber[] arr = new ComplexNumber[extendedSize];
-    String[] coeffs = br.readLine().split(" ");
-    
-    int i = 0;
+	static ComplexNumber[] getPolynomials(int extendedSize) throws IOException {
 
-    for(; i < coeffs.length; i++) {
-      arr[i] = new ComplexNumber(Integer.parseInt(coeffs[i]), 0);
-    }
-    for(; i < extendedSize; i++) {
-      arr[i] = new ComplexNumber(0, 0);
-    }
-    return arr;
-  }
+		ComplexNumber[] arr = new ComplexNumber[extendedSize];
+		String[] coeffs = br.readLine().split(" ");
 
-  static ComplexNumber[] TransformPolynomial(ComplexNumber[] poly, int size, int inversion) {
-    
-    if(size == 1) return poly;
-    
-    double exp = ((inversion * 2 * Math.PI) / size);
-    ComplexNumber wn = new ComplexNumber(1, 0);
-    ComplexNumber w1 = new ComplexNumber(Math.cos(exp), Math.sin(exp));
+		int i = 0;
 
-    ComplexNumber[] A0 = new ComplexNumber[size / 2];
-    ComplexNumber[] A1 = new ComplexNumber[size / 2];
+		for (; i < coeffs.length; i++) {
+			arr[i] = new ComplexNumber(Integer.parseInt(coeffs[i]), 0);
+		}
+		for (; i < extendedSize; i++) {
+			arr[i] = new ComplexNumber(0, 0);
+		}
+		return arr;
+	}
 
-    for (int i = 0; i < size / 2; i++) {
-      A0[i] = new ComplexNumber();
-      A0[i].real = poly[i * 2].real;
-      A0[i].img = poly[i * 2].img;
+	static ComplexNumber[] TransformPolynomial(ComplexNumber[] poly, int size, int inversion) {
 
+		if (size == 1)
+			return poly;
 
-      A1[i] = new ComplexNumber();
-      A1[i].real = poly[i * 2 + 1].real;
-      A1[i].img = poly[i * 2 + 1].img;
-    }
+		// double exp = ((inversion * 2 * Math.PI) / size);
+		// ComplexNumber wn = new ComplexNumber(1, 0);
+		// ComplexNumber w1 = new ComplexNumber(Math.cos(exp), Math.sin(exp));
+		ComplexNumber[] w = new ComplexNumber[size];
+		for (int i = 0; i < size; i++) {
+			double alpha = 2 * Math.PI * i / size;
+			w[i] = new ComplexNumber(Math.cos(alpha), Math.sin(alpha));
+		}
 
-    ComplexNumber[] Y0 = TransformPolynomial(A0, size / 2, inversion);
-    ComplexNumber[] Y1 = TransformPolynomial(A1, size / 2, inversion);
+		printArray(w);
 
-    ComplexNumber[] Y = new ComplexNumber[size];
+		ComplexNumber[] A0 = new ComplexNumber[size / 2];
+		ComplexNumber[] A1 = new ComplexNumber[size / 2];
 
-    for(int i = 0; i < size / 2; i++) {
-      ComplexNumber tmp1 = Y0[i].add(Y1[i].mul(wn));
+		for (int i = 0; i < size / 2; i++) {
+			A0[i] = new ComplexNumber();
+			A0[i].real = poly[i * 2].real;
+			A0[i].img = poly[i * 2].img;
 
-      Y[i] = new ComplexNumber();
-      Y[i].real = tmp1.real;
-      Y[i].img = tmp1.img;
+			A1[i] = new ComplexNumber();
+			A1[i].real = poly[i * 2 + 1].real;
+			A1[i].img = poly[i * 2 + 1].img;
+		}
 
-      ComplexNumber tmp2 = Y0[i].sub(Y1[i].mul(wn));
+		ComplexNumber[] Y0 = TransformPolynomial(A0, size / 2, inversion);
+		ComplexNumber[] Y1 = TransformPolynomial(A1, size / 2, inversion);
 
-      Y[(size / 2) + i] = new ComplexNumber();
-      Y[(size / 2) + i].real = tmp2.real;
-      Y[(size / 2) + i].img = tmp2.img;
+		ComplexNumber[] Y = new ComplexNumber[size];
 
-      ComplexNumber tmp3 = wn.mul(w1);
+		for (int i = 0; i < size / 2; i++) {
+			ComplexNumber tmp1 = Y0[i].add(Y1[i].mul(w[i]));
 
-      wn.real = tmp3.real;
-      wn.img = tmp3.img;
+			Y[i] = new ComplexNumber();
+			Y[i].real = tmp1.real;
+			Y[i].img = tmp1.img;
 
-    }
+			ComplexNumber tmp2 = Y0[i].sub(Y1[i].mul(w[i]));
 
-    return Y;
+			Y[(size / 2) + i] = new ComplexNumber();
+			Y[(size / 2) + i].real = tmp2.real;
+			Y[(size / 2) + i].img = tmp2.img;
 
-  }
+			// ComplexNumber tmp3 = wn.mul(w1);
 
-  static void multiplyPolynomials(ComplexNumber[] a, ComplexNumber[] b, ComplexNumber[] c, int size) {
-    for (int i = 0; i < size; i++) {
-      ComplexNumber product = a[i].mul(b[i]);
-      c[i] = new ComplexNumber();
-      c[i].real = product.real;
-      c[i].img = product.img;
-    }
-  }
+			// wn.real = tmp3.real;
+			// wn.img = tmp3.img;
 
-  public static void main(String[] args) throws IOException{
-    
-    int degA, degB; // with space between them
-    
-    String[] degrees = br.readLine().split(" ");
-    degA = Integer.parseInt(degrees[0]);
-    degB = Integer.parseInt(degrees[1]);
-    
-    int extendedSize = getExtendedSize(Math.max(degA, degB));
+		}
+		return Y;
 
-    ComplexNumber[] resPoly = new ComplexNumber[extendedSize];
+	}
 
-    ComplexNumber[] polyA = getPolynomials(extendedSize);
-    ComplexNumber[] polyB = getPolynomials(extendedSize);
+	static ComplexNumber[] multiplyPolynomials(ComplexNumber[] a, ComplexNumber[] b, ComplexNumber[] c, int size) {
+		for (int i = 0; i < size; i++) {
+			ComplexNumber product = a[i].mul(b[i]);
+			c[i] = new ComplexNumber();
+			c[i].real = product.real;
+			c[i].img = product.img;
+		}
+		return c;
+	}
 
-    ComplexNumber[] convertedA = TransformPolynomial(polyA, extendedSize, 1);
-    ComplexNumber[] convertedB = TransformPolynomial(polyB, extendedSize, 1);
-    
+	public static void main(String[] args) throws IOException {
 
-    printArray(convertedA);
-    printArray(convertedB);
+		int degA, degB; // with space between them
 
-    multiplyPolynomials(convertedA, convertedB, resPoly, extendedSize);
+		String[] degrees = br.readLine().split(" ");
+		degA = Integer.parseInt(degrees[0]);
+		degB = Integer.parseInt(degrees[1]);
 
-    ComplexNumber[] convertedRes = TransformPolynomial(resPoly, extendedSize, -1);
+		int extendedSize = getExtendedSize(degA + degB);
 
-    for (int i = 0; i < extendedSize; i++) {
-      convertedRes[i].real = convertedRes[i].real / extendedSize;
-      convertedRes[i].img = convertedRes[i].img / extendedSize;
-    }
+		// System.out.println("extendedSize " + extendedSize);
 
-    printArray(convertedRes);
-    
-    br.close();
-    ir.close();
-  }
+		ComplexNumber[] polyA = getPolynomials(extendedSize);
+		ComplexNumber[] polyB = getPolynomials(extendedSize);
+
+		printArray(polyA);
+		printArray(polyB);
+
+		ComplexNumber[] convertedA = TransformPolynomial(polyA, extendedSize, 1);
+		ComplexNumber[] convertedB = TransformPolynomial(polyB, extendedSize, 1);
+
+		printArray(convertedA);
+		printArray(convertedB);
+
+		ComplexNumber[] resPoly = new ComplexNumber[extendedSize];
+		multiplyPolynomials(convertedA, convertedB, resPoly, extendedSize);
+
+		ComplexNumber[] convertedRes = TransformPolynomial(resPoly, extendedSize, -1);
+
+		for (int i = 0; i < extendedSize; i++) {
+			convertedRes[i].real = convertedRes[i].real / extendedSize;
+			convertedRes[i].img = convertedRes[i].img / extendedSize;
+		}
+
+		printArray(convertedRes);
+
+		br.close();
+		ir.close();
+	}
 }
