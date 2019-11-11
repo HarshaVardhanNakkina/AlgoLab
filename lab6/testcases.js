703670ccfefc74c687866271114eaa6c875a65a8
HarshaVardhanNakkina <harshavardhan.n@gmail.com>
2019-11-11 15:54:40 +0530

    more testcases

--- lab6/testcases.js
+++ lab6/testcases.js
@@ -35,6 +35,29 @@ testcases = [
       {id: 4,profit: 77, weight: 22},
       {id: 5,profit: 90, weight: 25}
     ]
+  },
+  {
+    w: 15,
+    items: [
+      {id: 1, profit: 10, weight: 2},
+      {id: 2, profit: 5, weight: 3},
+      {id: 3, profit: 15, weight: 5},
+      {id: 4, profit: 7, weight: 7},
+      {id: 5, profit: 6, weight: 1},
+      {id: 6, profit: 18, weight: 4},
+      {id: 7, profit: 3, weight: 1}
+    ]
+  },
+  {
+    w: 16,
+    items: [
+      {id: 1, profit: 6, weight: 6},
+      {id: 2, profit: 2, weight: 10},
+      {id: 3, profit: 1, weight: 3},
+      {id: 4, profit: 8, weight: 5},
+      {id: 5, profit: 3, weight: 1},
+      {id: 6, profit: 5, weight: 3}
+    ]
   }
 ]
 module.exports = testcases
